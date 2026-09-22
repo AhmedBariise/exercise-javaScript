bffef3303612954f1632af45cbe8fb346bf27c4c
AhmedBariise <skillsinovative@gmail.com>
2026-09-22 10:04:43 +0300

    exercise 3

---
+++ exercise3/ex3.js
@@ -0,0 +1,28 @@
+let a = 15;
+let b = "15";
+
+// equal to 
+console.log(a == b);
+
+// not equal to 
+console.log(a != b);
+
+// strictly 
+console.log(a === b);
+
+// not strictly 
+console.log(a !==b );
+
+// greater than
+console.log(a > b);
+
+
+// less than
+console.log( a > b);
+
+
+// greater than or equal
+console.log(a >= b );
+
+// less than or equal 
+console.log(a <= b );
